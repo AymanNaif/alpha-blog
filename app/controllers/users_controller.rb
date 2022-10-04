1d099101dbad61b0f2cb09c6c653d1a78a1a38e7
AymanNaif <ayman_naif@hotmail.com>
2022-10-04 14:23:21 +0300

    add admin and it's funcionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
+
     def index
         @users = User.paginate(page: params[:page],per_page: 5) 
     end
@@ -13,8 +15,9 @@ class UsersController < ApplicationController
         @user = User.create(user_params)
 
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to the blog #{@user.username}!"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end
@@ -39,6 +42,14 @@ class UsersController < ApplicationController
         
         @user_article = @user.articles.paginate(page:params[:page],per_page:5)
     end
+
+    def destroy
+        @user = User.find(params[:id]) 
+        @user.destroy
+        flash[:danger] = "User and all articles created by user have been deleted" 
+        redirect_to users_path
+     end
+                
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
@@ -48,11 +59,19 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
     def require_same_user
-        if current_user != @user
-            flash[:danger] = "You can only edit your own account"  
+        if current_user != @user and !current_user.admin?
+            flash[:danger] = "You can only edit your own account"
             redirect_to root_path
         end
     end
         
+    def require_admin
+        if logged_in? and !current_user.admin?   
+            flash[:danger] = "Only admin users can perform that action"  
+            redirect_to root_path    
+        end
+    end
+        
+                
         
 end
