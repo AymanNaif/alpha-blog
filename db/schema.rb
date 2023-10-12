bcbdedc3ed7b2ab3219a3fd73e9531c2bffdb51b
Ayman Naif <ayman@zenhr.com>
2023-10-12 10:27:25 +0300

    create a user table and make an assoistaion between User and Article

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_20_074826) do
+ActiveRecord::Schema.define(version: 2023_10_12_071430) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
