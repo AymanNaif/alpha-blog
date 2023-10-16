db8db5d8be0612c5e394d304b69571c801f5cc1b
Ayman Naif <ayman@zenhr.com>
2023-10-16 12:01:39 +0300

    add password column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_12_071430) do
+ActiveRecord::Schema.define(version: 2023_10_16_085546) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2023_10_12_071430) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "password_digest"
   end
 
 end
